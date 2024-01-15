8eb75a48743e3cc7e842b48a935652f1ff19647d
Allyson Araujo <allyson.m.araujo@gmail.com>
2024-01-15 18:50:43 -0400

    segundo teste de código realizado com python

---
+++ main2.py
@@ -0,0 +1,14 @@
+# o André tem 28 anos de idade e mora na cidade de São Paulo
+
+
+
+nome = str(input('Digite um nome: '))
+idade = int(input('Digite a sua idade: '))
+try:
+  idade = int(idade)
+except ValueError:
+  print('Digite apenas números inteiros')
+cidade = str(input('Digite a cidade onde mora: '))
+
+print(f' {nome} tem {idade} anos de idade e mora na cidade de {cidade}')
+print(nome + ' tem ' + str(idade) + ' anos de idade e mora na cidade de '+ cidade)
---
+++ main3.py
@@ -0,0 +1,7 @@
+
+for n in range(1,500,20):
+  print(n)
+if (n > 0):
+  print()
+
+## asdasdasd
